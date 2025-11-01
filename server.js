8b29b4a30e14dfba369f2be77544198b19b9d5c5
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-11-01 09:54:10 +0530

    aaa3

--- server.js
+++ server.js
@@ -122,5 +122,7 @@ app.use(express.static(path.join(__dirname, "public")));
 //app.listen(3000, () => console.log("🌐 Server running at http://localhost:3000"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
